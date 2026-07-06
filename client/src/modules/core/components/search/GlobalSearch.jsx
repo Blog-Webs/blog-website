9c65a259077e6e4ed697a10ffa16b8b80601aeef
thetanish1 <tanishdewase25@gmail.com>
2026-07-06 18:52:12 +0530

    Reposition floating search bar to top-20 to sit directly below the sticky header

--- client/src/modules/core/components/search/GlobalSearch.jsx
+++ client/src/modules/core/components/search/GlobalSearch.jsx
@@ -65,7 +65,7 @@ export const GlobalSearch = () => {
     <div 
       className={`fixed left-1/2 -translate-x-1/2 z-[60] px-4 transition-all duration-300 ease-out transform ${
         isSticky 
-          ? 'top-4 opacity-100 translate-y-0 scale-100 pointer-events-auto' 
+          ? 'top-20 opacity-100 translate-y-0 scale-100 pointer-events-auto' 
           : '-top-20 opacity-0 -translate-y-4 scale-95 pointer-events-none'
       }`}
       style={{ width: '100%', maxWidth: '576px' }}
